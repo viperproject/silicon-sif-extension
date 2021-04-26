f14a02a9c94f894568dae6971d1954aebbe6c4f9
Marco Eilers <eilers.marco@googlemail.com>
2021-04-26 02:01:25 +0200

    Removed debug output

--- src/main/scala/CounterexampleTransformer.scala
+++ src/main/scala/CounterexampleTransformer.scala
@@ -14,12 +14,8 @@ trait CounterexampleSIFTransformer extends SIFExtendedTransformer {
     var newPrimeHeap: Seq[Chunk] = Seq() //ce.heap
     var newOldHeap: Option[Seq[Chunk]] = if (ce.oldHeap.isDefined) Some(Seq()) else None // ce.oldHeap
     var newPrimeOldHeap: Option[Seq[Chunk]] = if (ce.oldHeap.isDefined) Some(Seq()) else None // ce.oldHeap
-    println("ON LOOKUP, names for method " + methodName)
-    println("ARE " + primedNamesPerMethod.get(methodName).get)
     for ((n, np) <- primedNamesPerMethod.get(methodName).get) {
-      println("name in method " + n)
       if (ce.store.contains(n)) {
-        println("FOUND A VAR IN STORE " + n)
         // this is a variable
         val variableEntry = ce.internalStore.values.find(_._1.name == n).get
         newStore = newStore.+(variableEntry)
@@ -28,7 +24,6 @@ trait CounterexampleSIFTransformer extends SIFExtendedTransformer {
       }
     }
     for ((n, np) <- primedNames) {
-      println("primed name" + n)
 
       for (c <- ce.heap) {
         c match {
